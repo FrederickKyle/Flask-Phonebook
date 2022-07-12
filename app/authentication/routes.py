d07fcd9e0a4d480fe9f26feaf3cfa1ce60eb5723
Kyle Frederick <frederick.kyle1229@gmail.com>
2022-07-12 09:57:15 -0400

    first commit

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -1,4 +1,4 @@
-from crypt import methods
+
 from forms import UserLoginForm
 from models import User, db, check_password_hash
 from flask import Blueprint, render_template, request, redirect, url_for, flash
